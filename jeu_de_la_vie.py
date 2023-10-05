8f8b7d179ff095fec392d45269c64592607ed3fd
Bugxit <101186137+Bugxit@users.noreply.github.com>
2023-10-05 20:50:39 +0200

    Add files via upload

--- jeu_de_la_vie.py
+++ jeu_de_la_vie.py
@@ -1,23 +1,26 @@
 import turtle as tu
 import itertools as tool
+import time as ti
 
 tu.Screen().tracer(0)
-state_list = [[0 for x in range(int(tu.screensize()[0]/10))] for y in range(tu.screensize()[1])]
-object_list = []
+state_list = [[0 for x in range(int(tu.screensize()[0]/10)+1)] for y in range(int(tu.screensize()[1]/10)+1)]
 
 class Pixel():
 
     def __init__(self, coords, pixel_state, dead_neig, alive_neig):
         self.coords = coords
+        self.coords[0], self.coords[1] = self.coords[0]-1, self.coords[1]-1
         self.pixel_state = pixel_state
         self.dead_neig = dead_neig
         self.alive_neig = alive_neig
 
     def neig_state(self):
+        self.dead_neig = 0
+        self.alive_neig = 0
         for x_n, y_n in tool.product(range(3),range(3)):
-            if state_list[int((self.coords[0]+x_n-1)+(self.coords[1]+y_n-1)*(tu.screensize()[0]/10))] == 0:
+            if state_list[self.coords[1]+y_n-1][self.coords[0]+x_n-1] == 0:
                 self.dead_neig += 1
-        if state_list[int((self.coords[0])+(self.coords[1])*(tu.screensize()[0]/10))] == 0:
+        if state_list[self.coords[1]+y_n-1][self.coords[0]+x_n-1]== 0:
             self.dead_neig -=1
         self.alive_neig = 8-self.dead_neig
 
@@ -27,7 +30,7 @@ class Pixel():
         elif self.pixel_state == 1 and self.alive_neig not in [2,3]:
             self.pixel_state = 0
 
-    def pixel(self):
+    def draw_pixel(self):
         tu.up()
         tu.goto(self.coords[0]*10,self.coords[1]*10)
         tu.seth(0)
@@ -37,14 +40,19 @@ class Pixel():
             tu.forward(10)
         tu.end_fill()
 
-for x, y in tool.product(range(int(tu.screensize()[0]/10)), range(int(tu.screensize()[1]/10))):
-    object_list.append(Pixel((x,y), 0, 0, 0))
+state_list[10][9] = 1
+state_list[10][10] = 1
+state_list[10][11] = 1
 
 while True:
-    for i in range(len(object_list)):
-        object_list[i].neig_state()
-        object_list[i].change_pixel_state()
-        if object_list[i].pixel_state == 1:
-            object_list[i].pixel()
+    object_list = [[(Pixel([x,y], state_list[y][x], 0, 0)) for x in range(int(tu.screensize()[0]/10)+1)] for y in range(int(tu.screensize()[1]/10)+1)]
+    for x, y in tool.product(range(int(tu.screensize()[0]/10)+1), range(int(tu.screensize()[1]/10)+1)):
+        state_list[y][x] = object_list[y][x].pixel_state
+    for x, y in tool.product(range(int(tu.screensize()[0]/10)+1), range(int(tu.screensize()[1]/10)+1)):
+        object_list[y][x].neig_state()
+        object_list[y][x].change_pixel_state()
+        if object_list[y][x].pixel_state == 1:
+            object_list[y][x].draw_pixel()
+        state_list = []
     tu.Screen().update()
-
+    ti.sleep(1)
