eb84d4a3a27a7820dfd0c43352d901bd5da5e7d8
Bugxit <101186137+Bugxit@users.noreply.github.com>
2023-10-11 09:55:03 +0200

    Update jeu_de_la_vie.pyssssssssssssssssssssssssssssssssssss

--- jeu_de_la_vie.py
+++ jeu_de_la_vie.py
@@ -1,9 +1,12 @@
 import turtle as tu
 import math as m
 import itertools as tool
-import time as ti
 
+#Paramètrage de Turtle (Force FullScreen + Instant Update) + Variable de l'état du jeu
+tu.Screen().setup(width = 1.0, height = 1.0)
 tu.Screen().tracer(0)
+loop_started = False
+mouse_x, mouse_y = 0, 0
 
 class Pixel(tu.Turtle):
 
@@ -46,20 +49,39 @@ class Pixel(tu.Turtle):
             self.forward(10)
         self.end_fill()
 
+    def draw_button_pixel(self):
+        self.up()
+        self.goto(self.coords[0]*10-tu.window_width()/2, self.coords[1]*10-tu.window_height()/2)
+        self.seth(0)
+        self.down()
+        self.color('green')
+        self.begin_fill()
+        for i in range(4):
+            self.seth(i*90)
+            self.forward(10)
+        self.end_fill()
+
     def tick(self):
+        global loop_started
         self.clear()
-        tu.register_shape('10_10_square', ((-10, 0), (-10, 10), (0, 10), (0, 0)))
-        self.shape("10_10_square")
-        self.color('red')
-        self.goto(m.floor(mouse_x/10)*10, m.floor(mouse_y/10)*10)
-        self.stamp()
-        tu.Screen().ontimer(self.tick, 1000 // 30)
+        if loop_started == True:
+            loop()
+        else:
+            tu.register_shape('10_10_square', ((0, 0), (0, 10), (10, 10), (10, 0)))
+            self.shape("10_10_square")
+            self.color('red')
+            self.goto(m.floor(mouse_x/10)*10, m.floor((mouse_y)/10)*10+10)
+            self.stamp()
+            tu.Screen().ontimer(self.tick, 1000 // 30)
 
-tick_object = Pixel([-1,-1], 0, 0, 0)
+#Fonction qui calcul les coordonées du curseur
+def on_motion(event):
+    global mouse_x, mouse_y, loop_started
+    if loop_started == False:
+        mouse_x = event.x - tu.window_width() / 2
+        mouse_y = -event.y + tu.window_height() / 2
 
-state_list = [[0 for x in range(int(tu.window_width()/10)+2)] for y in range(int(tu.window_height()/10)+2)]
-object_list = [[(Pixel([x,y], state_list[y][x], 0, 0)) for x in range(int(tu.window_width()/10)+2)] for y in range(int(tu.window_height()/10)+2)]
-     
+#Fonction qui change les états des cellules lors d'un clic.
 def on_click_change(x,y):
     state_list[int((y+tu.window_height()/2)/10)][int((x+tu.window_width()/2)/10)] += 1
     object_list[int((y+tu.window_height()/2)/10)][int((x+tu.window_width()/2)/10)].pixel_state += 1
@@ -67,34 +89,41 @@ def on_click_change(x,y):
         state_list[int((y+tu.window_height()/2)/10)][int((x+tu.window_width()/2)/10)] = 0
         object_list[int((y+tu.window_height()/2)/10)][int((x+tu.window_width()/2)/10)].pixel_state = 0
         object_list[int((y+tu.window_height()/2)/10)][int((x+tu.window_width()/2)/10)].clear_pixel()
-        print('non')
     else:
         object_list[int((y+tu.window_height()/2)/10)][int((x+tu.window_width()/2)/10)].draw_pixel()
-        print('oui')
     tu.Screen().update()
 
-def on_motion(event):
-    global mouse_x, mouse_y
-    mouse_x = event.x - tu.window_width() / 2
-    mouse_y = -event.y + tu.window_height() / 2
+def loop():
+    global loop_started
+    loop_started = True
+    tu.listen()
+    while True:
+        for x, y in tool.product(range(int(tu.window_width()/10)), range(int(tu.window_height()/10))):
+            object_list[y+1][x+1].neig_state()
+            object_list[y+1][x+1].change_pixel_state()
+            if object_list[y+1][x+1].pixel_state == 1:
+                object_list[y+1][x+1].draw_pixel()
+            else:
+                object_list[y+1][x+1].clear_pixel()
+        for x, y in tool.product(range(int(tu.window_width()/10)), range(int(tu.window_height()/10))):
+            state_list[y+1][x+1] = object_list[y+1][x+1].pixel_state
+        tu.onkeypress(exit_func, 'space')
+        tu.Screen().update()
 
+def exit_func():
+    tu.bye()
+    exit()
+
+
+tick_object = Pixel([-1,-1], 0, 0, 0)
+
+#Création du tableau de jeu
+state_list = [[0 for x in range(int(tu.window_width()/10)+2)] for y in range(int(tu.window_height()/10)+2)]
+object_list = [[(Pixel([x,y], state_list[y][x], 0, 0)) for x in range(int(tu.window_width()/10)+2)] for y in range(int(tu.window_height()/10)+2)]
+
+tu.listen()
 tu.Screen().onclick(on_click_change)
-mouse_x, mouse_y = 0, 0
-tu.getcanvas().bind("<Motion>", on_motion)
+tu.onkeypress(loop, 'space') #Appelle la fonction Loop lorsque l'utilisateur appuie sur Espace
+tu.getcanvas().bind("<Motion>", on_motion)#Appelle la fonction on_motion lorsque l'utilisateur bouge le curseur de la souris
 tick_object.tick()
 tu.Screen().mainloop()
-
-while True:
-    for x, y in tool.product(range(int(tu.screensize()[0]/10)), range(int(tu.screensize()[1]/10))):
-        object_list[y+1][x+1].neig_state()
-        object_list[y+1][x+1].change_pixel_state()
-        if object_list[y+1][x+1].pixel_state == 1:
-            object_list[y+1][x+1].draw_pixel()
-        else:
-            object_list[y+1][x+1].clear_pixel()
-    for x, y in tool.product(range(int(tu.screensize()[0]/10)), range(int(tu.screensize()[1]/10))):
-        state_list[y+1][x+1] = object_list[y+1][x+1].pixel_state
-    tu.Screen().update()
-    ti.sleep(0.1)
-
-tu.exitonclick()
